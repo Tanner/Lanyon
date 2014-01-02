1098f69507cb885fae2d23ff7c5b8c1a6f68d716
Tanner Smith <tanner@tssoftware.net>
2014-01-02 12:05:34 -0500

    Initial Commit

---
+++ Lanyon/DHLDocument.h
@@ -0,0 +1,13 @@
+//
+//  DHLDocument.h
+//  Lanyon
+//
+//  Created by Tanner Smith on 1/2/14.
+//  Copyright (c) 2014 Tanner Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DHLDocument : NSDocument
+
+@end
---
+++ Lanyon/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Lanyon
+//
+//  Created by Tanner Smith on 1/2/14.
+//  Copyright (c) 2014 Tanner Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
